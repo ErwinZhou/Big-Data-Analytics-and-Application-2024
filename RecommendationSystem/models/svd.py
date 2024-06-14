Normalize The Data And Truncated Gradients And Fix Some Bugs

RecommendationSystem/models/svd.py
@@ -1,6 +1,7 @@
 from utils.config import np, pd, coo_matrix, csr_matrix, svds, EM, logging, tqdm, plt, pickle
 from utils.evaluation import SSE, RMSE
 from preprocessing.cache import load_from_pickle, save_to_pickle
+from utils.config import losses_png_path, models_path
 
 class SVD:
     """
@@ -9,15 +10,15 @@ class SVD:
     def __init__(self, num_factors=100, learning_rate=0.001, reg_bias=0.02, reg_pq=0.02, epochs=20, method="basis"):
         self.num_factors = num_factors  # The dimension of the latent space
         self.learning_rate = learning_rate
-        self.reg_bias = reg_bias # Regularization parameter for user and item biases to prevent overfitting
-        self.reg_pq = reg_pq # Regularization parameter for user and item latent factors to prevent overfitting
+        self.reg_bias = reg_bias  # Regularization parameter for user and item biases to prevent overfitting
+        self.reg_pq = reg_pq  # Regularization parameter for user and item latent factors to prevent overfitting
         self.epochs = epochs
         self.μ = None  # The global mean of the ratings
-        self.b_x = None # User biases [n_users] Treated as parameters to be learned
-        self.b_i = None # Item biases [n_items] Treated as parameters to be learned
+        self.b_x = None  # User biases [n_users] Treated as parameters to be learned
+        self.b_i = None  # Item biases [n_items] Treated as parameters to be learned
         self.P = None  # User matrix of the SVD [n_users, num_factors]
         self.Q = None  # Item matrix of the SVD [n_items, num_factors]
-        self.method = method # The method to extract the latent factors, either 'basis' or 'IncSVD'
+        self.method = method  # The method to extract the latent factors, either 'basis' or 'IncSVD'
         self.losses = []  # List to store the RMSE for each epoch
 
         # Set up logging
@@ -97,27 +98,28 @@ class SVD:
                 epoch_errors.append(error)
 
                 # Update biases
-                self.b_x[user] += self.learning_rate * (error - self.reg_bias * self.b_x[user])
-                if user == 5 and item == 369927:
-                    print("self.b_x[user]: ", self.b_x[user])
-                self.b_i[item] += self.learning_rate * (error - self.reg_bias * self.b_i[item])
-                if user == 5 and item == 369927:
-                    print("self.b_i[item]: ", self.b_i[item])
+                b_x_grad = self.learning_rate * (error - self.reg_bias * self.b_x[user])
+                b_i_grad = self.learning_rate * (error - self.reg_bias * self.b_i[item])
+
                 # Update latent factors
-                self.P[user, :] += self.learning_rate * (error * self.Q[item, :] - self.reg_pq * self.P[user, :])
-                if user == 5 and item == 369927:
-                    print("self.P[user, :]: ", self.P[user, :])
-                self.Q[item, :] += self.learning_rate * (error * self.P[user, :] - self.reg_pq * self.Q[item, :])
-                if user == 5 and item == 369927:
-                    print("self.Q[item, :]: ", self.Q[item, :])
-                # Prevent overflow and NaNs
-                self.P[user, :] = np.clip(self.P[user, :], -1e10, 1e10)
-                self.Q[item, :] = np.clip(self.Q[item, :], -1e10, 1e10)
+                P_grad = self.learning_rate * (error * self.Q[item, :] - self.reg_pq * self.P[user, :])
+                Q_grad = self.learning_rate * (error * self.P[user, :] - self.reg_pq * self.Q[item, :])
+
+                # Clip gradients
+                b_x_grad = np.clip(b_x_grad, -1.0, 1.0)
+                b_i_grad = np.clip(b_i_grad, -1.0, 1.0)
+                P_grad = np.clip(P_grad, -1.0, 1.0)
+                Q_grad = np.clip(Q_grad, -1.0, 1.0)
+
+                # Apply gradients
+                self.b_x[user] += b_x_grad
+                self.b_i[item] += b_i_grad
+                self.P[user, :] += P_grad
+                self.Q[item, :] += Q_grad
 
             rmse = np.sqrt(np.mean(np.square(epoch_errors)))
             self.losses.append(rmse)
             self.logger.info(f"Epoch {epoch + 1} completed. RMSE: {rmse:.4f}")
-    
 
     def plot_losses(self):
         """
@@ -129,7 +131,8 @@ class SVD:
         plt.xlabel('Epoch')
         plt.ylabel('RMSE')
         plt.grid(True)
-        plt.show() 
+        plt.savefig(f'{losses_png_path}_{self.num_factors}_{self.learning_rate}_{self.reg_bias}_{self.reg_pq}_{self.epochs}_{self.method}.png')
+        plt.show()
        
     def predict_single(self, user, item):
         """
@@ -139,10 +142,12 @@ class SVD:
         :param item: int, the item ID
         :return: float, the predicted rating
         """
-        # prediction = self.μ + self.b_x[user] + self.b_i[item] + np.dot(self.P[user, :], self.Q[item, :])
-        prediction = self.μ + self.b_x[user] + self.b_i[item]
-        if prediction > 100 :
-            prediction = 100
+        prediction = self.μ + self.b_x[user] + self.b_i[item] + np.dot(self.P[user, :], self.Q[item, :])
+        # prediction = self.μ + self.b_x[user] + self.b_i[item]
+        if prediction > 10:
+            prediction = 10
+        elif prediction < 0:
+            prediction = 0
         return prediction
 
     def predict(self, user_id, item_id):
@@ -178,17 +183,45 @@ class SVD:
         with open(path, 'wb') as f:
             pickle.dump(model_data, f)
         self.logger.info(f"Model saved to {path}")
+    
+    def load_model(self, path):
+        """
+        Load the trained model from a file.
+        :param path: str, the file path to load the model
+        """
+        with open(path, 'rb') as f:
+            model_data = pickle.load(f)
+        self.num_factors = model_data['num_factors']
+        self.learning_rate = model_data['learning_rate']
+        self.reg_bias = model_data['reg_bias']
+        self.reg_pq = model_data['reg_pq']
+        self.epochs = model_data['epochs']
+        self.μ = model_data['μ']
+        self.b_x = model_data['b_x']
+        self.b_i = model_data['b_i']
+        self.P = model_data['P']
+        self.Q = model_data['Q']
+        self.logger.info(f"Model loaded from {path}")
+
 
 
 if __name__ == "__main__":
 
     user_ratings = load_from_pickle('data/cache/pkls/user_ratings.pkl')
     # Initialize and train the SVD model
-    svd = SVD(num_factors=20, learning_rate=0.005, reg_bias=0.02, reg_pq=0.02, epochs=15, method = "IncSVD")
+    num_factors = 20
+    learning_rate = 0.005
+    reg_bias = 0.02
+    reg_pq = 0.02
+    epochs = 1
+    method = "basis"
+    svd = SVD(num_factors=num_factors, learning_rate=learning_rate, reg_bias=reg_bias, reg_pq=reg_pq, epochs=epochs, method = method)
     svd.fit(user_ratings)
-    
-    # Save the model
-    svd.save_model('data/svd_model.pkl')
+
+    # Save the model on name includes all the hyperparameters
+    model_name = f'{models_path}_{num_factors}_{learning_rate}_{reg_bias}_{reg_pq}_{epochs}_{method}.pkl'
+
+    svd.save_model(model_name)
 
     # Example prediction
     user_id = 0

RecommendationSystem/utils/helpers.py
@@ -1,6 +1,11 @@
 from preprocessing.cache import save_to_pickle, load_from_pickle
 from utils.config import np, pd, defaultdict, plt, colors, math
 
+def log_normalization(rating):
+    return np.log1p(rating)  # log(1 + rating)
+
+def min_max_normalization(rating, min_rating, max_rating, new_min=1, new_max=10):
+    return (rating - min_rating) / (max_rating - min_rating) * (new_max - new_min) + new_min
 
 def get_user_data(file_path):
     """
@@ -33,6 +38,7 @@ def analyze_training_data(user_ratings, train_data_stats_path, train_data_rating
     - [Items] Number of rated items, max item id, min item id
     - [Ratings] Number of ratings, max rating, min rating, average rating, missing values, variance, sparseness ratio
     - [Rating Distribution] Distribution of ratings
+    - [Others] (1) Is there some users never rated any items? 
     Args:
         user_ratings: dict, the user ratings
         train_data_stats_path: str, the path of the statistics file
@@ -65,6 +71,8 @@ def analyze_training_data(user_ratings, train_data_stats_path, train_data_rating
     variance = rating_series.var() if all_ratings else None
     sparseness_ratio = 1 - (total_ratings / (max_user_id * max_item_id))
 
+    # Is there some users never rated any items?
+    users_never_rated = [user_id for user_id, ratings in user_ratings.items() if not ratings]
 
     # Save statistics to a text file
     try:
@@ -88,6 +96,8 @@ def analyze_training_data(user_ratings, train_data_stats_path, train_data_rating
             print(f'  Sparseness ratio: {sparseness_ratio:.2%}', file=f)
             print('\nRating distribution:', file=f)
             print('\n'.join(f'  {line}' for line in str(rating_distribution).split('\n')), file=f)
+            print('\nOthers: ', file=f)
+            print(f'  Users never rated any items: {users_never_rated}', file=f)
         
         # Plot the rating distribution with bins
         plt.hist(rating_series, bins=10, edgecolor='black', color='grey', alpha=0.5) if all_ratings else None
@@ -207,7 +217,9 @@ def analyze_attribute_data(attribute_data, attribute_stats_path, attribute_distr
     It includes the following aspects:
     - [Attributes] Max and min values of the attributes, Number of None values, ratio of None values, average value of the attributes, variance
     - [Attribute Distribution] Distribution of the attributes
-    - [Others] If the itemAttribute.txt file includes all the item 
+    - [Others] (1)If the itemAttribute.txt file includes all the item 
+               (2)The range of items in the itemAttribute.txt file
+               (3)If the items are continuous
     Args:
         attribute_data: dict, the attribute data
         attribute_stats_path: str, the path of the statistics file
@@ -237,6 +249,14 @@ def analyze_attribute_data(attribute_data, attribute_stats_path, attribute_distr
     var_attribute1 = np.var(attribute1_values) if attribute1_values else None
     var_attribute2 = np.var(attribute2_values) if attribute2_values else None
 
+    # (2)The range of items in the itemAttribute.txt file
+    max_item_id = max(attribute_data.keys())
+    min_item_id = min(attribute_data.keys())
+    # (3)If the items are continuous
+    continuous = False
+    if len(attribute_data) == max_item_id - min_item_id + 1:
+        continuous = True
+
     try:
         with open(attribute_stats_path, 'w') as file:
             print(f'Attribute 1: ', file=file)
@@ -256,7 +276,10 @@ def analyze_attribute_data(attribute_data, attribute_stats_path, attribute_distr
             print('\n', file=file)
             print(f'Total number of None values: {total_none_values}', file=file)
             print(f'Total ratio of None values: {total_none_ratio:.2f}%', file=file)  
-    
+            print('\nOthers: ', file=file)
+            print(f'  The range of items in the itemAttribute.txt file: {min_item_id} - {max_item_id}', file=file)
+            print(f'  If the items are continuous: {continuous}', file=file)
+
         bins = np.linspace(min(min(attribute1_values), min(attribute2_values)), 
                            max(max(attribute1_values), max(attribute2_values)), 11)
 
@@ -331,17 +354,32 @@ def load_training_data(train_data_path, index_map):
         True or False 
     """
     user_ratings, item_raters = defaultdict(list), defaultdict(list)
+    
+    log_ratings = []
+    # First pass to apply log transformation and collect all log ratings
     with open(train_data_path, 'r') as file:
         while (line := file.readline().strip()):
             user_id, num_ratings = map(int, line.split('|'))
             for _ in range(num_ratings):
                 line = file.readline().strip()
                 item_id, rating = map(int, line.split())
-                normalized_rating = rating / 10.0 
+                log_rating = log_normalization(rating)
+                log_ratings.append(log_rating)
+
+    min_log_rating = np.min(log_ratings)
+    max_log_rating = np.max(log_ratings)
+    
+    # Second pass to normalize log ratings to the desired range
+    with open(train_data_path, 'r') as file:
+        while (line := file.readline().strip()):
+            user_id, num_ratings = map(int, line.split('|'))
+            for _ in range(num_ratings):
+                line = file.readline().strip()
+                item_id, rating = map(int, line.split())
+                log_rating = log_normalization(rating)
+                normalized_rating = min_max_normalization(log_rating, min_log_rating, max_log_rating, 0, 10)
                 user_ratings[user_id].append([index_map[item_id], normalized_rating])
                 item_raters[index_map[item_id]].append([user_id, normalized_rating])
-                # user_ratings[user_id].append([index_map[item_id], rating]) 
-                # item_raters[index_map[item_id]].append([user_id, rating]) # Only if the item is previously stored in the index map and rated by the users
     
     # Save the user_ratings and item_raters to txt files
     with open('data/cache/txts/user_ratings.txt', 'w') as file:
@@ -355,7 +393,6 @@ def load_training_data(train_data_path, index_map):
             print(f'{item_id} {len(raters)}', file=file)
             for user_id, rating in raters:
                 print(f'{user_id} {rating}', file=file)
-    
 
     # Save the user_ratings and item_raters to pikle files
     if save_to_pickle(user_ratings, 'data/cache/pkls/user_ratings.pkl') and save_to_pickle(item_raters, 'data/cache/pkls/item_raters.pkl'):